77b4df041338a43722932fce4267558d08cba4d5
jaesma <jamesmanalang11392@gmail.com>
2019-05-08 15:15:12 +0800

    Implementing Post Redirect Get Pattern

--- src/main/java/com/jaesma/controller/TodoItemController.java
+++ src/main/java/com/jaesma/controller/TodoItemController.java
@@ -8,9 +8,12 @@ import com.jaesma.util.Mappings;
 import com.jaesma.util.ViewNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class TodoItemController {
@@ -36,8 +39,16 @@ public class TodoItemController {
         return ViewNames.ITEMS_LIST;
     }
 
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model) {
+        TodoItem todoItem = new TodoItem("","", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
+        todoItemService.addItem(todoItem);
         return "redirect:/" + Mappings.ITEMS;
     }
 }
